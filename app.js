4acd568128714717272e11e6730e6fd8eeab721a
Mikko Kasanen <mikko.kasanen@microsoft.com>
2020-10-28 09:11:21 +0200

    added string function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { Console } = require('console');
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -5,6 +6,10 @@ var path = require('path');
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index3.html'));
+});
+
+app.get('/string', function(req, res) {
+    Console.log("hello");
 });
 
 app.listen(8080);
