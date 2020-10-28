789365bb2d4bd5581b978c7e69d2e65de7055e44
Mikko Kasanen <mikko.kasanen@microsoft.com>
2020-10-28 09:02:28 +0200

    v3

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index2.html'));
+    res.sendFile(path.join(__dirname + '/index3.html'));
 });
 
 app.listen(8080);
